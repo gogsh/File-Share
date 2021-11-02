e7dcbeadc80f7acbbbd1b17ac86cbc0c2ddbac59
Ilya <IlyaAbdullinWork@yandex.ru>
2021-11-02 22:24:32 +0500

    socker listners

---
+++ server/socker/sockerListeners.ts
@@ -0,0 +1,18 @@
+import * as socketio from 'socket.io'
+import { Rooms } from '../model/room'
+import { createRoom, joinRoom } from './sockerControllers'
+
+export const createRoomListener = (socket: socketio.Socket, rooms: Rooms) => {
+  socket.on('LOBBY:CREATE', ({ roomId }) => {
+    createRoom(rooms, roomId)
+    // socket.emit('TEST', {data: 'data'})
+  })
+}
+
+export const joinRoomListener = (socket: socketio.Socket, rooms: Rooms) => {
+  socket.on('LOBBY:JOIN', ({ roomId }) => {
+    socket.join(roomId)
+    joinRoom(rooms, roomId, socket.id)
+    // socket.emit('LOBBY:JOIN', { lul: 'lul' })
+  })
+}
